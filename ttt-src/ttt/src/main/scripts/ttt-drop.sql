2df511e4d6800a434d3f85ab9e93b76ee1122af0
ronak1790 <ronak1790@gmail.com>
2015-03-31 14:38:18 -0700

    modify
    modify

--- ttt-src/ttt/src/main/scripts/ttt-drop.sql
+++
@@ -1,9 +0,0 @@
-
-drop table game_values;
-drop table game_users_games;
-drop table games;
-
-drop table authorities;
-drop table game_users;
-
-drop sequence hibernate_sequence;
--- ttt-src/ttt/src/main/webapp/WEB-INF/jsp/home.jsp
+++
@@ -1,6 +0,0 @@
-<html>
-<head><title>Spring MVC Example</title></head>
-<body>
-Welcome to Spring MVC.
-</body>
-</html>
